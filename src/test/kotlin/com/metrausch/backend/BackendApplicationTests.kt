4069c26c77e23b8ba3db6f5ef72cc1456c6aec60
anseto <anseto1988.iwers@gmail.com>
2025-02-07 18:28:22 +0100

    init

---
+++ src/test/kotlin/com/metrausch/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.metrausch.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
